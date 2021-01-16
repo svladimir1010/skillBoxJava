d3a452b65e4ce7b3dd2adc2aeaa03960234e6289
Vladimir <vladimireur@gmail.com>
2021-01-16 12:37:32 +0200

    Add LambdaExpressions

---
+++ LambdaExpressions/src/Main.java
@@ -0,0 +1,44 @@
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.text.SimpleDateFormat;
+import java.util.*;
+
+public class Main {
+    private static String staffFile = "data/staff.txt";
+    private static String dateFormat = "dd.MM.yyyy";
+
+    public static void main(String[] args) {
+        ArrayList<Employee> staff = loadStaffFromFile();
+        //============================ одновременное сравнение по двум параметрам с "Lambda"
+        Collections.sort(staff, Comparator.comparing(Employee::getSalary)
+                .thenComparing(Employee::getName));
+        //============================
+
+        for (Employee employee : staff) {
+            System.out.println(employee);
+        }
+    }
+
+    private static ArrayList<Employee> loadStaffFromFile() {
+        ArrayList<Employee> staff = new ArrayList<>();
+        try {
+            List<String> lines = Files.readAllLines(Paths.get(staffFile));
+            for (String line : lines) {
+                String[] fragments = line.split("\t");
+                if (fragments.length != 3) {
+                    System.out.println("Wrong line: " + line);
+                    continue;
+                }
+
+                staff.add(new Employee(
+                        fragments[0],
+                        Integer.parseInt(fragments[1]),
+                        (new SimpleDateFormat(dateFormat)).parse(fragments[2])
+                ));
+            }
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+        return staff;
+    }
+}
