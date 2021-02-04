160ead593b41a926d6d5120cf7328aab80b43031
Vladimir <vladimireur@gmail.com>
2021-02-04 19:25:49 +0200

    Example logging with log24j2

--- UnitTests/src/main/java/Main.java
+++ UnitTests/src/main/java/Main.java
@@ -1,5 +1,12 @@
+
 import core.Line;
 import core.Station;
+
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.Marker;
+import org.apache.logging.log4j.MarkerManager;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
@@ -10,50 +17,57 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
-public class Main
-{
+
+public class Main {
+    public static Logger loggerInfo;
+    public static Logger loggerWarn;
+    private static Logger loggerError;
+
     private static String dataFile = "src/main/resources/map.json";
     private static Scanner scanner;
+    private static Marker CORRECT_STATION = MarkerManager.getMarker("INPUT_CORRECT");
+    private static Marker WRONG_STATION = MarkerManager.getMarker("WRONG_INPUT");
 
     private static StationIndex stationIndex;
 
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
         RouteCalculator calculator = getRouteCalculator();
+
+        loggerInfo = LogManager.getLogger("InfoLog");
+        loggerWarn = LogManager.getLogger("WarnLog");
+        loggerError = LogManager.getLogger("ErrorLog");
 
         System.out.println("Программа расчёта маршрутов метрополитена Санкт-Петербурга\n");
         scanner = new Scanner(System.in);
-        for(;;)
-        {
-            Station from = takeStation("Введите станцию отправления:");
-            Station to = takeStation("Введите станцию назначения:");
+        try {
+            for (; ; ) {
+                Station from = takeStation("Введите станцию отправления:");
+                Station to = takeStation("Введите станцию назначения:");
 
-            List<Station> route = calculator.getShortestRoute(from, to);
-            System.out.println("Маршрут:");
-            printRoute(route);
+                List<Station> route = calculator.getShortestRoute(from, to);
+                System.out.println("Маршрут:");
+                printRoute(route);
 
-            System.out.println("Длительность: " +
-                    RouteCalculator.calculateDuration(route) + " минут");
+                System.out.println("Длительность: " +
+                        RouteCalculator.calculateDuration(route) + " минут");
+            }
+        } catch (Exception ex) {
+            loggerError.error(ex.getMessage(), ex);
         }
     }
 
-    private static RouteCalculator getRouteCalculator()
-    {
+    private static RouteCalculator getRouteCalculator() {
         createStationIndex();
         return new RouteCalculator(stationIndex);
     }
 
-    private static void printRoute(List<Station> route)
-    {
+    private static void printRoute(List<Station> route) {
         Station previousStation = null;
-        for(Station station : route)
-        {
-            if(previousStation != null)
-            {
+        for (Station station : route) {
+            if (previousStation != null) {
                 Line prevLine = previousStation.getLine();
                 Line nextLine = station.getLine();
-                if(!prevLine.equals(nextLine))
-                {
+                if (!prevLine.equals(nextLine)) {
                     System.out.println("\tПереход на станцию " +
                             station.getName() + " (" + nextLine.getName() + " линия)");
                 }
@@ -63,25 +77,23 @@ public class Main
         }
     }
 
-    private static Station takeStation(String message)
-    {
-        for(;;)
-        {
+    private static Station takeStation(String message) {
+        for (; ; ) {
             System.out.println(message);
             String line = scanner.nextLine().trim();
             Station station = stationIndex.getStation(line);
-            if(station != null) {
+            if (station != null) {
+                loggerInfo.info(CORRECT_STATION, "User was search: " + station.getName());
                 return station;
             }
+            loggerWarn.warn(WRONG_STATION, "Station not found " + line);
             System.out.println("Станция не найдена :(");
         }
     }
 
-    private static void createStationIndex()
-    {
+    private static void createStationIndex() {
         stationIndex = new StationIndex();
-        try
-        {
+        try {
             JSONParser parser = new JSONParser();
             JSONObject jsonData = (JSONObject) parser.parse(getJsonFile());
 
@@ -93,14 +105,12 @@ public class Main
 
             JSONArray connectionsArray = (JSONArray) jsonData.get("connections");
             parseConnections(connectionsArray);
-        }
-        catch(Exception ex) {
+        } catch (Exception ex) {
             ex.printStackTrace();
         }
     }
 
-    private static void parseConnections(JSONArray connectionsArray)
-    {
+    private static void parseConnections(JSONArray connectionsArray) {
         connectionsArray.forEach(connectionObject ->
         {
             JSONArray connection = (JSONArray) connectionObject;
@@ -112,8 +122,7 @@ public class Main
                 String stationName = (String) itemObject.get("station");
 
                 Station station = stationIndex.getStation(stationName, lineNumber);
-                if(station == null)
-                {
+                if (station == null) {
                     throw new IllegalArgumentException("core.Station " +
                             stationName + " on line " + lineNumber + " not found");
                 }
@@ -123,8 +132,7 @@ public class Main
         });
     }
 
-    private static void parseStations(JSONObject stationsObject)
-    {
+    private static void parseStations(JSONObject stationsObject) {
         stationsObject.keySet().forEach(lineNumberObject ->
         {
             int lineNumber = Integer.parseInt((String) lineNumberObject);
@@ -139,8 +147,7 @@ public class Main
         });
     }
 
-    private static void parseLines(JSONArray linesArray)
-    {
+    private static void parseLines(JSONArray linesArray) {
         linesArray.forEach(lineObject -> {
             JSONObject lineJsonObject = (JSONObject) lineObject;
             Line line = new Line(
@@ -151,14 +158,12 @@ public class Main
         });
     }
 
-    private static String getJsonFile()
-    {
+    private static String getJsonFile() {
         StringBuilder builder = new StringBuilder();
         try {
             List<String> lines = Files.readAllLines(Paths.get(dataFile));
             lines.forEach(line -> builder.append(line));
-        }
-        catch (Exception ex) {
+        } catch (Exception ex) {
             ex.printStackTrace();
         }
         return builder.toString();
